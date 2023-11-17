d00aaf032e100e2cd9c504cba1e37b983a85c6b8
Terryroud <108944688+Terryroud@users.noreply.github.com>
2023-11-17 14:54:46 +0300

    Update final.py

--- final.py
+++ final.py
@@ -92,7 +92,7 @@ for i in a:
 c = c[9:]
 
 model1 = keras.models.load_model('model1')
-y = ['Food', 'Cat']#я хз как это сделать ну типо в массив поместить тэги которые юзер хочет
+y = ['Food', 'Cat']
 res = []
 z = empty
 for j in y:
